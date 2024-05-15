ce5e4d5fce3a5733c618120d2cd9b60705daf73a
Taylor Ng <taylormng95@gmail.com>
2024-05-15 13:56:26 -0400

    completes event listening lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const button = document.querySelector('#button');
+    button.addEventListener('click', clickAlert)
+
+    function clickAlert(){
+        alert('I was clicked!');
+    }
 }
